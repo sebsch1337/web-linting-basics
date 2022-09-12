72c05a55931e1f7bb8d9ea4d98c954530b7e363d
Sebastian Scherbes <github@scherbes.de>
2022-09-12 16:07:28 +0200

    fixed toggle buttons

--- js/toggle.js
+++ js/toggle.js
@@ -1,14 +1,14 @@
-function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
+export default function toggleButton() {
+  const cards = document.querySelectorAll('[data-js="card"]');
+  //const button = cards.querySelectorAll('[data-js="button"]');
   cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hide');
+      arrow.classList.toggle('rotate');
+    });
+  });
+  console.log('test');
 }
